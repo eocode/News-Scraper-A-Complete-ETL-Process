9895b98f297585a7ee0984a9d1091925aeffdec6
Elias Ojeda Medina <eliasojedamedina@gmail.com>
2020-04-29 13:17:52 -0500

    :rocket: Basic structure for scrapper

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 > * Data Engineering ETL Process
 
 # Features
+* Page Object Pattern for build this solution
 * Scraping News Data from universal.com.mx
 
 # Preview
--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+import argparse
+import logging
+
+from common import config
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def _news_scraper(news_site_uid):
+    host = config()['news_sites'][news_site_uid]['url']
+    logging.info(f'Beginning scraper for {host}')
+
 
 if __name__ == '__main__':
-    print('Hola mundo')
+    parser = argparse.ArgumentParser()
+    news_site_choices = list(config()['news_sites'].keys())
+    parser.add_argument('news_site',
+                        help='The news site that you want to scrape',
+                        type=str,
+                        choices=news_site_choices)
+
+    args = parser.parse_args()
+    _news_scraper(args.news_site)
